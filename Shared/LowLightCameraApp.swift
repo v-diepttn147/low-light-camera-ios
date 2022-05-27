7fe6f74e07bae8486c5a1942c135416a02b37964
Diep Tran <diepttn147@gmail.com>
2022-05-27 21:28:55 +0700

    Initial Commit

---
+++ Shared/LowLightCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LowLightCameraApp.swift
+//  Shared
+//
+//  Created by Diep Tran on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct LowLightCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
